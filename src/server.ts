6396bd7cedc28e22fd9dba23f435e875824bb727
Shanto93 <shantoislam7363@gmail.com>
2025-06-19 19:34:19 +0600

    chore: set up project with MongoDB, Mongoose, and TypeScript

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { Server } from "http";
+import app from "./app";
+import "dotenv/config";
+const port = process.env.PORT;
+
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(`${process.env.URL}`);
+    console.log("Connected to MongoDB");
+    const server = app.listen(port, () => {
+      console.log(`Library Management app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
