6d06acd2888c6510dcb90724c35ab82c8a5b8a04
Shanto93 <shantoislam7363@gmail.com>
2025-06-19 22:08:45 +0600

    add endpoint to fetch single book by ID

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -3,15 +3,15 @@ import { Book } from "../models/book.model";
 
 export const bookRoutes = express.Router();
 
+// Create a new book
 bookRoutes.post("/", async (req: Request, res: Response) => {
   const body = req.body;
-  // Assuming you have a Book model imported
   try {
     const book = await Book.create(body);
     res.status(201).json({
       success: true,
       message: "Book created successfully",
-      book: book,
+      data: book,
     });
   } catch (error: any) {
     res.status(400).json({
@@ -21,13 +21,14 @@ bookRoutes.post("/", async (req: Request, res: Response) => {
     });
   }
 });
+
+// GET all books
 bookRoutes.get("/", async (req: Request, res: Response) => {
   const filter = req.query.filter as string | undefined;
   console.log(filter);
   const sortBy = req.query.sortBy as string | undefined;
   const sort = req.query.sort as string | undefined;
   const limit = parseInt(req.query.limit as string) || 10;
-  console.log(sort, sortBy, filter, limit);
 
   try {
     const book = await Book.find(filter ? { genre: filter } : {})
@@ -36,12 +37,30 @@ bookRoutes.get("/", async (req: Request, res: Response) => {
     res.status(201).json({
       success: true,
       message: "Books retrieved successfully",
-      book: book,
+      data: book,
     });
   } catch (error: any) {
     res.status(400).json({
       success: false,
       message: "Error fetching books",
+      error: error.message,
+    });
+  }
+});
+// GET a sngle book
+bookRoutes.get("/:bookId", async (req: Request, res: Response) => {
+  const bookId = req.params.bookId;
+  try {
+    const book = await Book.findById(bookId);
+    res.status(201).json({
+      success: true,
+      message: "Book retrieved successfully",
+      data: book,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: "Error fetching book",
       error: error.message,
     });
   }
