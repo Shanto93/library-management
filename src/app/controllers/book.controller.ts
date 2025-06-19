fd6da6cc160a68d2616634a0046c7b30d6bb7096
Shanto93 <shantoislam7363@gmail.com>
2025-06-19 21:50:32 +0600

    GET all books and enable filtering, sorting, and limit for books list

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -21,3 +21,28 @@ bookRoutes.post("/", async (req: Request, res: Response) => {
     });
   }
 });
+bookRoutes.get("/", async (req: Request, res: Response) => {
+  const filter = req.query.filter as string | undefined;
+  console.log(filter);
+  const sortBy = req.query.sortBy as string | undefined;
+  const sort = req.query.sort as string | undefined;
+  const limit = parseInt(req.query.limit as string) || 10;
+  console.log(sort, sortBy, filter, limit);
+
+  try {
+    const book = await Book.find(filter ? { genre: filter } : {})
+      .sort(sortBy ? { [sortBy]: sort === "desc" ? -1 : 1 } : {})
+      .limit(limit);
+    res.status(201).json({
+      success: true,
+      message: "Books retrieved successfully",
+      book: book,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: "Error fetching books",
+      error: error.message,
+    });
+  }
+});
